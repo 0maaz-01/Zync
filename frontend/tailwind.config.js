bb21b6101c1716a2bf6e0e121538d7f931318cf1
Mohammed Maaz Rayeen <mmaaz0510@gmail.com>
2026-03-14 15:27:09 +0530

    Add files via upload

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,11 +1,27 @@
+import defaultTheme from "tailwindcss/defaultTheme";
 
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-            colors: {
+      colors: {
         background: "#000000",
+      },
+      screens: {
+        ...defaultTheme.screens, // ⬅️ brings back sm, md, lg, xl, 2xl
+        'xxs': '350px',
+        'xs': '450px',
+        'lx': '1120px',
+        'ml' : "840px"
+      },
+      boxShadow: {
+        'button': '0 0px 15px rgba(136, 14, 79, 1)',   // 'button': '0 0px 20px rgba(136, 14, 79, 1)',
+        '3xl': '0 0px 40px rgba(136, 14, 79, 1)',
+        'bar':'0 0px 10px rgba(136, 14, 79, 1)',
+        'screen': '0 0px 20px rgba(136, 14, 79, 1)',
+        'confirm': '0 0px 20px rgba(234, 179, 8, 1)',
+        'recording': "0 0px 60px rgba(234, 179, 8, 1)"
       },
     },
   },
