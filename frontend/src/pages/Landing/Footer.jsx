be5621fc26815cefe762cef16c4ee675464914d5
Mohammed Maaz Rayeen <mmaaz0510@gmail.com>
2026-03-14 15:32:39 +0530

    Add files via upload

---
+++ frontend/src/pages/Landing/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+      <footer className="relative z-1 border-t py-4 border-white/60 text-white">
+        <div className=" text-center " >
+          <p>© 2025 Zync | <a className="  py-1 px-4 ml-2 bg-purple-700 hover:bg-purple-700/90 " href="https://www.maazverse.com/">Built by Maaz</a></p>
+        </div>
+      </footer>
+  )
+}
+
+export default Footer
