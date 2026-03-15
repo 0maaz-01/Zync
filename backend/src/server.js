e19b745bddbb55cda652b61123a37cd691bbfce3
Mohammed Maaz Rayeen <mmaaz0510@gmail.com>
2026-03-15 21:32:18 +0530

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,23 +2,17 @@ import express from "express";
 import "dotenv/config";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import path from "path";
-
 import authRoutes from "./routes/auth.route.js";
 import userRoutes from "./routes/user.route.js";
 import chatRoutes from "./routes/chat.route.js";
-
 import { connectDB } from "./lib/db.js";
-
 const app = express();
 const PORT = process.env.PORT;
 
-const __dirname = path.resolve();
-
 app.use(
   cors({
-    origin: "http://localhost:5173",
-    credentials: true, // allow frontend to send cookies
+    origin: "http://localhost:5173/",
+    credentials: true, 
   })
 );
 
@@ -29,15 +23,9 @@ app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/chat", chatRoutes);
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-  });
-}
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
   connectDB();
 });
+
+
